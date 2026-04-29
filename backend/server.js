902c0d08f714609c91bba54875c31116f3bd7562
PyetroNatalino <pnunesnatalino@gmail.com>
2026-04-29 19:46:52 -0300

    corrigindo conexão mongo Atlas

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+require("dotenv").config(); 
 
 const app = express();
-const SECRET = "minha-chave-secreta";
+const SECRET = process.env.JWT_SECRET || "minha-chave-secreta";
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/painel")
+mongoose.connect("mongodb+srv://PyetroNatalino:%40Estarossa04@cluster0.je8c2ih.mongodb.net/painel")
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
 
-// Model
 const ClienteSchema = new mongoose.Schema({
   nome: String
 });
 
 const Cliente = mongoose.model("Cliente", ClienteSchema);
 
-// LOGIN
+
 app.post("/auth/login", (req, res) => {
   const { email, password } = req.body;
 
-  if (email === "PyetroNatalino" && password === "122456") {
+  if (email === "pnunesnatalino@gmail.com" && password === "122456") {
     const token = jwt.sign(
       { user: email },
       SECRET,
@@ -38,9 +37,9 @@ app.post("/auth/login", (req, res) => {
   return res.status(401).json({ message: "Login inválido" });
 });
 
-// Middleware JWT
+
 function autenticarToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers["authorization"]; 
 
   const token = authHeader && authHeader.split(" ")[1];
 
@@ -50,33 +49,35 @@ function autenticarToken(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, SECRET);
-    req.user = decoded;
+    req.user = decoded; 
     next();
   } catch (err) {
     return res.status(403).json({ message: "Token inválido" });
   }
 }
 
-// GET clientes
 app.get("/clientes", autenticarToken, async (req, res) => {
   const clientes = await Cliente.find();
   res.json(clientes);
 });
 
-// POST cliente
 app.post("/clientes", autenticarToken, async (req, res) => {
   const novo = new Cliente({ nome: req.body.nome });
   await novo.save();
   res.json(novo);
 });
 
-// DELETE cliente
 app.delete("/clientes/:id", autenticarToken, async (req, res) => {
   await Cliente.findByIdAndDelete(req.params.id);
   res.send("ok");
 });
 
-// Server
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta" + PORT);
 });
